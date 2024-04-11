c6a18033b6641d4aaec947cbb78697a75dacfc38
Survila Kernius <kernius.survila@mif.stud.vu.lt>
2024-04-11 12:04:58 +0300

    socket.io nginx config & change baseurl

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,16 +1,7 @@
 export const getBaseUrl = () =>
   process.env.NODE_ENV === "production"
-    ? "https://hivenote.lol"
+    ? "https://hivenote.xyz"
     : "http://localhost:4000";
 
-// export const getSocketBaseUrl = (noteId) =>
-//   (process.env.NODE_ENV === "production"
-//     ? "hivenote.lol"
-//     : "192.168.0.173:9092") +
-//   "?room=" +
-//   noteId;
 export const getSocketBaseUrl = () =>
-  process.env.NODE_ENV === "production" ? "hivenote.lol" : "localhost:9092";
-// process.env.NODE_ENV === "production" ? "hivenote.lol" : "192.168.64.1:9092";
-// process.env.NODE_ENV === "production" ? "hivenote.lol" : "127.0.0.1:9092";
-// process.env.NODE_ENV === "production" ? "hivenote.lol" : "192.168.0.173:9092";
+  process.env.NODE_ENV === "production" ? "hivenote.xyz" : "localhost:9092";
