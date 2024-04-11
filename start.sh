c04a9d92f3d3681be8adcda4c04d661210c915cf
Survila Kernius <kernius.survila@mif.stud.vu.lt>
2024-04-11 11:02:42 +0300

    cred fix + migrate to aws ecr

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-docker pull kerniussur/hivenote-backend
-docker pull kerniussur/hivenote-frontend
+aws ecr get-login-password | docker login --username hivenote --password-stdin 992382369361.dkr.ecr.eu-central-1.amazonaws.com
+docker pull 992382369361.dkr.ecr.eu-central-1.amazonaws.com/hivenote-backend
+docker pull 992382369361.dkr.ecr.eu-central-1.amazonaws.com/hivenote-frontend
 docker-compose up -d
