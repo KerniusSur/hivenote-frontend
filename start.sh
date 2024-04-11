c8edb7311a27eb7206a821f4279c4136d1ad39e6
Survila Kernius <kernius.survila@mif.stud.vu.lt>
2024-04-11 11:09:07 +0300

    cred fix

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-aws ecr get-login-password | docker login --username hivenote --password-stdin 992382369361.dkr.ecr.eu-central-1.amazonaws.com
+aws ecr get-login-password | docker login --username AWS --password-stdin 992382369361.dkr.ecr.eu-central-1.amazonaws.com
 docker pull 992382369361.dkr.ecr.eu-central-1.amazonaws.com/hivenote-backend
 docker pull 992382369361.dkr.ecr.eu-central-1.amazonaws.com/hivenote-frontend
 docker-compose up -d
