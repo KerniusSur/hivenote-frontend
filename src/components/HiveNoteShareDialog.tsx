27382b8dd286fda5c180ac24e17de38767a02161
Survila Kernius <kernius.survila@mif.stud.vu.lt>
2024-04-30 17:30:51 +0300

    add note sharing dialog draft

---
+++ src/components/HiveNoteShareDialog.tsx
@@ -0,0 +1,54 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  useMediaQuery,
+} from "@mui/material";
+import { useState } from "react";
+
+interface HiveNoteShareDialogProps {
+  open: boolean;
+  handleClose: () => void;
+  handleSubmit: () => void;
+}
+
+const HiveNoteShareDialog = (props: HiveNoteShareDialogProps) => {
+  const { open, handleClose, handleSubmit } = props;
+  const isMobile = useMediaQuery('(max-width: 600px)');
+
+  const onsSubmit = () => {
+    handleSubmit();
+    handleClose();
+  };
+
+  return (
+    <Dialog
+      open={open}
+      sx={{
+        width: '100%',
+      }}
+      PaperProps={{
+        sx: {
+          maxWidth: isMobile ? '100%' : '1000px',
+          maxHeight: isMobile ? '100%' : '817px',
+          width: '100%',
+          borderRadius: '12px',
+          margin: isMobile ? '0' : '2rem',
+          height: '100%',
+          overflowX: 'hidden',
+        },
+      }}
+    >
+      <DialogTitle>Edit Note</DialogTitle>
+      <DialogContent></DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button onClick={onsSubmit}>Save</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default HiveNoteShareDialog;
